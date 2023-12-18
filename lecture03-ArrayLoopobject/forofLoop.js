dcb04592577d75238fd946966e9283570be3ef9c
Pan153001 <kumakumakumaBEAR01>
2023-12-18 16:40:05 +0700

    first commit

---
+++ lecture03-ArrayLoopobject/forofLoop.js
@@ -0,0 +1,5 @@
+const names = ['Justuin', 'Sarah', 'Christopher'];
+
+for(let name of names){
+    console.log(name);
+}
---
+++ lecture03-ArrayLoopobject/iterator1.js
@@ -0,0 +1,14 @@
+let str = "Hello";
+
+let iterator = str[Symbol.iterator]();
+
+while(true){
+    let result = iterator.next();
+    if(result.done) break;
+    console.log(result.value);
+}
+
+
+
+//for(let char of "test"){
+    //console.log( char );}
