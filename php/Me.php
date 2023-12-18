b9247a9b9e239083c656d34c77c9588321f80bec
Pan153001 <kumakumakumaBEAR01>
2023-12-18 18:20:14 +0700

    commit

--- php/Me.php
+++ php/Me.php
@@ -22,17 +22,23 @@
     <table width = "50%">
         <td>
         <?php
+            $name = "นายปัณณวัฒน์ อัญชลิสังกาศ";
+            $nickname = "ปัณ";
+            $age = 19;
+            $hobby = "เล่นเกม";
+            $branch = "IT";
+            $Ayear = 2;
 
-            echo "<h1>Introduce Myself </h1><br/>";
+            echo "<h1>Introduce Myself </h1>";
 
             echo "<div align = 'center'><img src ='cat.webp' width = '40%' height = '40%'><br>";
 
-            echo "<p class ='font'>ชื่อ-นามสกุล : นายปัณณวัฒน์ อัญชลิสังกาศ <br>";
-            echo "<p class ='font'>ชื่อเล่น : ปัณ <br>";
-            echo "<p class ='font'>อายุ : 19 <br/>";
-            echo "<p class ='font'>งานอดิเรก : เล่นเกม <br>";
-            echo "<p class ='font'>สาขา : IT <br>";
-            echo "<p class ='font'>ชั้นปีที่ : 2 <br><br>";
+            echo "<p class ='font'>ชื่อ-นามสกุล : $name <br>";
+            echo "<p class ='font'>ชื่อเล่น : $nickname <br>";
+            echo "<p class ='font'>อายุ : $age <br/>";
+            echo "<p class ='font'>งานอดิเรก : $hobby <br>";
+            echo "<p class ='font'>สาขา : $branch <br>";
+            echo "<p class ='font'>ชั้นปีที่ : $Ayear <br><br>";
             echo "<font size = '6'><p><a href ='travel.html' style='text-decoration: none'><b>Travel";
 
         ?>
