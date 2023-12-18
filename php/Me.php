3938785ca67344ee800c0cf9ab28fdbe159eca98
Pan153001 <kumakumakumaBEAR01>
2023-12-18 17:55:12 +0700

    commit

---
+++ php/Me.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="me.css">
+    <title>Introduce Myself</title>
+    <style>
+
+        h1 {
+            text-align: center;
+            color: black; 
+            background-color: yellow;
+            font-size: 50px;
+        }
+
+    </style>
+</head>
+<body>
+    <div align = "center">
+
+    <table width = "50%">
+        <td>
+        <?php
+
+            echo "<h1>Introduce Myself </h1><br/>";
+
+            echo "<div align = 'center'><img src ='cat.webp' width = '40%' height = '40%'><br>";
+
+            echo "<p class ='font'>ชื่อ-นามสกุล : นายปัณณวัฒน์ อัญชลิสังกาศ <br>";
+            echo "<p class ='font'>ชื่อเล่น : ปัณ <br>";
+            echo "<p class ='font'>อายุ : 19 <br/>";
+            echo "<p class ='font'>งานอดิเรก : เล่นเกม <br>";
+            echo "<p class ='font'>สาขา : IT <br>";
+            echo "<p class ='font'>ชั้นปีที่ : 2 <br><br>";
+            echo "<font size = '6'><p><a href ='travel.html' style='text-decoration: none'><b>Travel";
+
+        ?>
+        </td>
+    </table>
+    </div>
+</body>
+</html>
