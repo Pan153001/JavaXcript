7196c0593513ea7a62eafff03d095898f6e02a7e
Pan153001 <kumakumakumaBEAR01>
2024-01-25 22:38:41 +0700

    commit

--- html/test1-cal.php
+++ html/test1-cal.php
@@ -20,6 +20,25 @@
         echo "<font color ='#66FF00'>*</font><br>";
     }
     ?>
+    <?php
+        echo "<pre>";
+        for ($i = 1; $i < 8; $i++) {
+            for ($j = $i; $j < 8; $j++)
+                echo "&nbsp;&nbsp;";
+            for ($j = 2 * $i - 1; $j > 0; $j--)
+                echo ("&nbsp;*");
+            echo "<br>";
+        }
+        $n = 8;
+        for ($i = 8; $i > 0; $i--) {
+            for ($j = $n - $i; $j > 0; $j--)
+                echo "&nbsp;&nbsp;";
+            for ($j = 2 * $i - 1; $j > 0; $j--)
+                echo ("&nbsp;*");
+            echo "<br>";
+        }
+        echo "</pre>";
+    ?>
     </td>
 
     </table>
